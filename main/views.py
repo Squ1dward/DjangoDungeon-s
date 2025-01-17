d08facdf93327c6894785c9c48e253ce1120cb7f
H3rzius <h3rzius@gmail.com>
2025-01-17 11:29:33 +0100

    Add template view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from django.views.generic import TemplateView
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+    return HttpResponse("",loader.get_template("polls/index.html"))
+
