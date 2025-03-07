eeff5cdbc7ea4fe843a93c18da06fd0584760250
H3rzius <h3rzius@gmail.com>
2025-03-07 10:47:43 +0100

    add register and ai api key to gpt

--- main/views.py
+++ main/views.py
@@ -5,9 +5,11 @@ from django.utils import timezone
 
 from .models import User, ChatProfile, ChatMessage
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm, ChatForm
+from .forms import LoginForm, ChatForm, RegisterForm
 
 from django.views.generic import TemplateView
+import requests
+import json
 
 
 # Create your views here.
@@ -29,11 +31,10 @@ def index(request):
             message = form.cleaned_data['text_field']
             chat_message = ChatMessage(profileId=chat_profile, userId_id=request.user.id, message=message, postDate=timezone.now())
             chat_message.save()
-            # TODO: Armin soll hier seinem GPT-Prompt coden. Der Antwort unter chat_message_from_gpt -> message='{{Hier dein GPT-Antwort eingeben}}' implementieren
             # -> Get response from GPT
-            #response_from_gpt = '{{Hier dein GPT-Antwort eingeben}}'
-            #chat_message_from_gpt = ChatMessage(profileId=chat_profile.id, userId_id=0, message=response_from_gpt, postDate=timezone.now())
-            #chat_message_from_gpt.save()
+            response_from_gpt = get_gpt(message)
+            chat_message_from_gpt = ChatMessage(profileId=chat_profile, userId_id=0, message=response_from_gpt, postDate=timezone.now())
+            chat_message_from_gpt.save()
             chat_profile = ChatProfile.objects.get(createdBy=request.user.id)
             chat_messages = ChatMessage.objects.filter(profileId=chat_profile.id).values()
             messages_dict = {
@@ -75,6 +76,24 @@ def login_view(request):
 
     return render(request, 'main/login_view.html', {'form': form, 'error_message': error_message})
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            new_chat_profile = ChatProfile(creationDate=timezone.now(), createdBy=user)
+            new_chat_profile.save()
+            chat_profile = ChatProfile.objects.get(createdBy=user)
+            new_message_from_gpt = ChatMessage(profileId=chat_profile, userId=User.objects.get(id=0), message=get_gpt(None), postDate=timezone.now())
+            new_message_from_gpt.save()
+            return redirect('index')
+    else:
+        form = RegisterForm()
+    return render(request, 'main/register.html', {'form': form})
+
 def logout_view(request):
     logout(request)
     return redirect('/')
@@ -84,6 +103,32 @@ def delete_chat_view(request):
     chat_messages = ChatMessage.objects.filter(profileId=chat_profile.id)
     for chat_message in chat_messages:
         chat_message.delete()
-    chat_message = ChatMessage(profileId=chat_profile, userId=User.objects.get(id=0), message="You need to save the village from evil goblins, they choose you as the only one goblin-hunter. You need to defeat the goblin-chief. Do you want to join the adventure?", postDate=timezone.now())
+    chat_message = ChatMessage(profileId=chat_profile, userId=User.objects.get(id=0), message=get_gpt(None), postDate=timezone.now())
     chat_message.save()
     return redirect('/')
+
+def get_gpt(user_prompt):
+    if user_prompt is None:
+        user_prompt = "You're a DungeonMaster, you create an dungeon scenario and I'm the main character. You start the story. Send also another text summaring in words what you wrote"
+    response = requests.post(
+      url="https://openrouter.ai/api/v1/chat/completions",
+      headers={
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json",
+        "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+        "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+      },
+      data=json.dumps({
+        "model": "cognitivecomputations/dolphin3.0-mistral-24b:free",
+        "messages": [
+          {
+            "role": "user",
+            "content": user_prompt
+          }
+        ],
+
+      })
+    )
+    response_data = response.json()
+    first_response = response_data['choices'][0]['message']['content']
+    return first_response
